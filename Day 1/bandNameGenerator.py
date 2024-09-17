492d5b6b21a78ee0b35a7f3a1988c852b9036d73
kiyotamonica <monica.kiyota@gmail.com>
2024-09-17 16:11:38 -0300

    Day 1 :tada:

---
+++ Day 1/bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator application ! :)")
+city_name = input("What's your home town's name ?\n")
+pet_name = input("What's your pet's name ?\n")
+print("New band name:\n" + city_name + "'s " + pet_name)
